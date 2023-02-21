5961bd2e00f0ea8b6e9eb0a898de3e8ab03be3b7
Eren <aeyilmaz@uwaterloo.ca>
2023-02-21 13:27:06 -0500

    real-time recording

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
-from ImageRenamingClasses import BinaryNameAssigner
+from imageRenamingClasses import BinaryNameAssigner
 from imageTimingClasses import imageTiming
 
 # ask user for names and store them in an array
-responses = [1, 0, 1, 0, 1]
+
+# example array passed in
+responses = [1, 0, 1, 0]
+
 bna = BinaryNameAssigner(responses)
 assigned_names = bna.assign()
 
